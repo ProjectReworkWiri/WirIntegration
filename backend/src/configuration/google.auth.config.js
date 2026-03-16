5edc565122eec8ac808f1546ff7e9fd52f41c334
TheDarkness6859 <suarezgarcia939@gmail.com>
2026-03-16 09:54:21 -0500

    fix google auth

--- backend/src/configuration/google.auth.config.js
+++ backend/src/configuration/google.auth.config.js
@@ -35,10 +35,16 @@ passport.serializeUser((user, done) => {
 passport.deserializeUser(async (id, done) => {
   try {
 
+    if (!id) return done(null, false);
+
     const user = await pool.query(
       "SELECT * FROM users WHERE id=$1",
       [id]
     );
+
+    if (result.rows.length === 0) {
+      return done(null, false); 
+    }
 
     done(null, user.rows[0]);
 
